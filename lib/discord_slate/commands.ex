abb936a278843e2fe7ba61f2b6c2fe033c088749
rekyuu <senkan.re.kyuu@gmail.com>
2018-09-02 22:15:00 -0600

    color stuff

--- lib/discord_slate/commands.ex
+++ lib/discord_slate/commands.ex
@@ -1,7 +1,7 @@
 defmodule DiscordSlate.Commands do
   defmacro __using__(_opts) do
     quote do
-      import DiscordSlate.Commands.{Admin, Announce}
+      import DiscordSlate.Commands.{Admin, Announce, Color}
     end
   end
 end
