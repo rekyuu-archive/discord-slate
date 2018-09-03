cbe9e200e31dea67a67ad0f0976d36ad5e13641d
rekyuu <senkan.re.kyuu@gmail.com>
2018-09-02 22:22:37 -0600

    response to color setting

--- lib/discord_slate/commands/color.ex
+++ lib/discord_slate/commands/color.ex
@@ -17,30 +17,42 @@ defmodule DiscordSlate.Commands.Color do
   def set_color_red(data) do
     guild_id = Channel.get(data.channel_id).guild_id    
     Guild.add_member_role(guild_id, data.author.id, 352605883278163989)
+
+    reply "Done!"
   end
 
   def set_color_orange(data) do
     guild_id = Channel.get(data.channel_id).guild_id    
     Guild.add_member_role(guild_id, data.author.id, 479314401300578344)
+
+    reply "Done!"
   end
 
   def set_color_yellow(data) do
     guild_id = Channel.get(data.channel_id).guild_id    
     Guild.add_member_role(guild_id, data.author.id, 486022292745224203)
+
+    reply "Done!"
   end
 
   def set_color_green(data) do
     guild_id = Channel.get(data.channel_id).guild_id    
     Guild.add_member_role(guild_id, data.author.id, 486022159454699540)
+
+    reply "Done!"
   end
 
   def set_color_blue(data) do
     guild_id = Channel.get(data.channel_id).guild_id    
     Guild.add_member_role(guild_id, data.author.id, 479314520695767040)
+
+    reply "Done!"
   end
 
   def set_color_purple(data) do
     guild_id = Channel.get(data.channel_id).guild_id    
     Guild.add_member_role(guild_id, data.author.id, 352603354259521536)
+
+    reply "Done!"
   end
 end
